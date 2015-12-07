46d9ac6e3843e17d8aa3c1471e3b2b27382ef2c1
Lance <Lance@thelpstory.com>
2015-12-07 16:35:23 -0700

    security issue

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
-    MONGO_URI: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/bipo',
-    SALT_WORK_FACTOR: 10,
-
-};
