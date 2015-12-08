7b92bff373b95bf7b3615e4a44c9e53add39c119
Lance <Lance@thelpstory.com>
2015-12-08 16:04:38 -0700

    created altconfig to hold pointers to env variables

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 
 
 // *** config file *** //
-var config = require('../../_config' || '../altConfig');
+var config = require('../_config' || './altConfig');
 
 
 // *** express instance *** //
