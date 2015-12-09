6244bce5758f384c6184003d618f68fa3bacbc2e
Lance <Lance@thelpstory.com>
2015-12-08 19:49:07 -0700

    insurance chart working

--- client/components/chart/chart.directive.js
+++ client/components/chart/chart.directive.js
@@ -21,5 +21,53 @@
     function ChartController() {
         var vm = this;
         console.log('chart controller')
+
+        vm.title = "Insurance Report"
+        vm.chart = nv.models.multiBarChart();
+
+        d3.select('#chart svg').datum([
+            {
+                key: "Out-Of-Pocket",
+                color: "#BD362F",
+
+                values: [{
+                    x: "Talk Therapy",
+                    y: 1000
+                    }, {
+                    x: "Medications",
+                    y: 1500
+                    }, {
+                    x: "Intensive Outpatient",
+                    y: 3500
+                    }, {
+                    x: "Electro-Convulsive",
+                    y: 8000
+                    }, {
+                    x: "Inpatient",
+                    y: 8000
+                    }]
+            },
+            {
+                key: "Insurance",
+                color: "#51A351",
+                values: [{
+                        x: "Talk Therapy",
+                        y: 1500
+                    }, {
+                        x: "Medications",
+                        y: 3500
+                    }, {
+                        x: "Intensive Outpatient",
+                        y: 5000
+                    }, {
+                        x: "Electro-Convulsive",
+                        y: 9000
+                    },
+                    {
+                        x: "Inpatient",
+                        y: 13000
+                        }, ]
+            }]).transition().duration(500).call(vm.chart);
+
     }
 })();
