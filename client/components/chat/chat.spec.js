d93cfd6b77d19f98025b620d1226fb180dda0dff
Lance <Lance@thelpstory.com>
2015-12-12 14:00:47 -0700

    changed testing specs

---
+++ client/components/chat/chat.spec.js
@@ -0,0 +1,5 @@
+describe('sorting the list of users', function () {
+    it('sorts in descending order by default', function () {
+        expect(true).toEqual(true);
+    });
+});
--- conf.js
+++ conf.js
@@ -2,8 +2,6 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     suites: {
-        chart: 'client/components/chart/chart.spec.js',
-        form: 'client/components/userForm/userForm.spec.js',
-        grid: 'client/components/grid/grid.spec.js'
+        chat: 'client/components/chat/chat.spec.js',
     }
 }
